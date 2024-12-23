2708e37d758b615f2ab1a7a0f39f69330a5c7847
Akmal Ihksan <146054817+Soedjamal@users.noreply.github.com>
2024-12-23 13:11:51 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   }),
